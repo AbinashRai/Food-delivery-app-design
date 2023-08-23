49994bd3db5d4c4dce740c987578ac3ba983cc87
Abinash Rai <abinashbantawa1905@gmail.com>
2023-08-23 17:35:24 +0545

    added all the images

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Notifications = () => {
   return (
-    <div className="h-12 bg-red-500 text-white px-4 flex items-center text-center justify-center">
+    <div className="h-12 bg-red-500 text-white px-4 flex items-center text-center justify-center text-sm md:text-base cursor-pointer">
       Free delivery to all our customers for the first month! Call now for a
-      quick order! 
+      quick order!
     </div>
   );
 };
