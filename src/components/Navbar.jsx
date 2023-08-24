dfd9421f2ffaff155ed7d80eec8e04eef37708cf
Abinash Rai <abinashbantawa1905@gmail.com>
2023-08-24 16:54:36 +0545

    navbar done for the medium screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,16 @@ const Navbar = () => {
   return (
     <>
       <div className="h-12 flex text-red-500 p-4 justify-between items-center border-b-2 border-b-red-500 uppercase">
+        <div className="hidden md:flex gap-4">
+          <Link href="/">Homepage</Link>
+          <Link href="/menu">Menu</Link>
+          <Link href="/">Contact</Link>
+        </div>
+
         <div className="text-xl">
           <Link href="/">Massimo</Link>
         </div>
-        <div>
+        <div className="md:hidden">
           <Menu />
         </div>
       </div>
